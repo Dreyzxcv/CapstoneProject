b424dd040d127d1c49ddabe90632e4d9ff6b336e
Kasokee <frijholytz08@gmail.com>
2026-07-03 17:43:58 +0800

    Fixed: jev issue

--- app/Actions/IssueJev.php
+++ app/Actions/IssueJev.php
@@ -18,7 +18,7 @@ class IssueJev
         protected AuditLogService $auditLogService,
     ) {}
 
-    public function execute(Asset $asset, string $jevNumber, User $accountingUser): Jev
+    public function execute(Asset $asset, array $data, User $accountingUser): Jev
     {
         if ($asset->current_status !== AssetStatus::ClearedForAccounting) {
             throw new DomainException('Asset is not cleared for accounting.');
@@ -28,10 +28,20 @@ class IssueJev
             throw new DomainException('JEV already exists for this asset.');
         }
 
-        return DB::transaction(function () use ($asset, $jevNumber, $accountingUser) {
+        return DB::transaction(function () use ($asset, $data, $accountingUser) {
             $jev = Jev::create([
                 'asset_id' => $asset->id,
-                'jev_number' => $jevNumber,
+                'jev_number' => $data['jev_number'],
+                'funding_source_code' => $data['funding_source_code'] ?? null,
+                'funding_source_label' => $data['funding_source_label'] ?? null,
+                'transaction_type' => $data['transaction_type'] ?? null,
+                'transaction_code' => $data['transaction_code'] ?? null,
+                'responsibility_center' => $data['responsibility_center'] ?? null,
+                'particulars' => $data['particulars'] ?? null,
+                'document_no' => $data['document_no'] ?? null,
+                'prepared_by_name' => $data['prepared_by_name'] ?? $accountingUser->name,
+                'approved_by_name' => $data['approved_by_name'] ?? null,
+                'line_items' => $data['line_items'] ?? null,
                 'created_by_accounting_id' => $accountingUser->id,
             ]);
 
