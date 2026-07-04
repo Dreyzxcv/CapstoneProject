4ce60aa71f8e17bbf9ade8e65c17db3c5a9ae0a6
Kasokee <frijholytz08@gmail.com>
2026-07-04 21:07:11 +0800

    Add: Adding UpdateUserRequest

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->can('users.manage');
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                Rule::unique(User::class)->ignore($this->route('user')),
+            ],
+            'role' => ['required', 'string', 'exists:roles,name'],
+        ];
+    }
+}
