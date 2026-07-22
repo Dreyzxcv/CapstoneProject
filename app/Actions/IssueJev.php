be850756424e9fe8138df446d10fffe3921bc30b
Kasokee <frijholytz08@gmail.com>
2026-07-22 17:39:17 +0800

    Modified: removing unnecessary info for JEV

--- app/Actions/IssueJev.php
+++ app/Actions/IssueJev.php
@@ -7,14 +7,12 @@ use App\Models\Asset;
 use App\Models\Jev;
 use App\Models\User;
 use App\Services\AuditLogService;
-use App\Services\PdfDocumentService;
 use DomainException;
 use Illuminate\Support\Facades\DB;
 
 class IssueJev
 {
     public function __construct(
-        protected PdfDocumentService $pdfDocumentService,
         protected AuditLogService $auditLogService,
     ) {}
 
@@ -32,20 +30,8 @@ class IssueJev
             $jev = Jev::create([
                 'asset_id' => $asset->id,
                 'jev_number' => $data['jev_number'],
-                'funding_source_code' => $data['funding_source_code'] ?? null,
-                'funding_source_label' => $data['funding_source_label'] ?? null,
-                'transaction_type' => $data['transaction_type'] ?? null,
-                'transaction_code' => $data['transaction_code'] ?? null,
-                'responsibility_center' => $data['responsibility_center'] ?? null,
-                'particulars' => $data['particulars'] ?? null,
-                'document_no' => $data['document_no'] ?? null,
-                'prepared_by_name' => $data['prepared_by_name'] ?? $accountingUser->name,
-                'approved_by_name' => $data['approved_by_name'] ?? null,
-                'line_items' => $data['line_items'] ?? null,
                 'created_by_accounting_id' => $accountingUser->id,
             ]);
-
-            $this->pdfDocumentService->generateJev($asset, $jev);
 
             $this->auditLogService->log('jev.issued', $jev, null, $jev->toArray(), $accountingUser->id);
 
