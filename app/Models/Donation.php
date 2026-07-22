ada8b174e5fe4dfeccdc848e94cf91b62506b09b
Kasokee <frijholytz08@gmail.com>
2026-07-22 12:04:28 +0800

    Modified: adding types in donation

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -10,16 +10,22 @@ class Donation extends Model
     protected $fillable = [
         'disposal_id',
         'requester_name',
+        'organization_type',
+        'organization_type_other',
+        'agency_name',
         'deed_of_donation_path',
+        'release_photo_path',
         'released_at',
     ];
 
     protected function casts(): array
     {
         return [
+            'organization_type' => \App\Enums\DonationOrganizationType::class,
             'released_at' => 'datetime',
         ];
     }
+
 
     public function disposal(): BelongsTo
     {
