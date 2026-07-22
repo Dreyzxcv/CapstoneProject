296e2e9111e7acc9e233c1c0522ab1b86c3a5c96
Kasokee <frijholytz08@gmail.com>
2026-07-22 12:05:04 +0800

    Modified: adding types in donation

--- app/Http/Requests/ProcessDisposalRequest.php
+++ app/Http/Requests/ProcessDisposalRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use App\Enums\DisposalType;
 use Illuminate\Foundation\Http\FormRequest;
+use App\Enums\DonationOrganizationType;
 use Illuminate\Validation\Rule;
 
 class ProcessDisposalRequest extends FormRequest
@@ -20,6 +21,9 @@ class ProcessDisposalRequest extends FormRequest
             'quantity' => ['nullable', 'integer', 'min:1'],
             'requester_name' => ['required_if:disposal_type,donation', 'nullable', 'string', 'max:255'],
             'delivery_coordinates' => ['nullable', 'string', 'max:255'],
+            'organization_type' => ['required_if:disposal_type,donation', Rule::enum(DonationOrganizationType::class)],
+            'organization_type_other' => ['required_if:organization_type,other', 'nullable', 'string', 'max:255'],
+            'agency_name' => ['nullable', 'required_unless:organization_type,individual', 'string', 'max:255'],
             'appeal_filed' => ['nullable', 'boolean'],
             'details' => ['nullable', 'array'],
             'notes' => ['nullable', 'string', 'max:1000'],
