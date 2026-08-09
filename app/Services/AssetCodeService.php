3caba4bafab443506549c4efbbe8999d3467198d
Kasokee <frijholytz08@gmail.com>
2026-08-09 11:58:03 +0800

    Modified: date of apprehension -> date report submitted

--- app/Services/AssetCodeService.php
+++ app/Services/AssetCodeService.php
@@ -9,7 +9,7 @@ class AssetCodeService
 {
     public function generate(Asset $asset, Municipality $municipality, bool $hasOngoingCase): string
     {
-        $year = $asset->incident?->date_of_apprehension?->format('Y') ?? now()->format('Y');
+        $year = $asset->incident?->date_report_submitted?->format('Y') ?? now()->format('Y');
         $sequence = str_pad((string) $asset->id, 5, '0', STR_PAD_LEFT);
 
         return 'AAP-FV-'.$year.'-'.$sequence;
